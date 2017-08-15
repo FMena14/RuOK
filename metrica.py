afa7e969105f11e5234652b54df1fd4710c2dfda
FMena14 <francisco.mena.13@sansano.usm.cl>
2017-08-15 04:32:37 -0300

    recuperar

---
+++ metrica.py
@@ -0,0 +1,105 @@
+import pandas as pd
+import numpy as np
+from os import walk
+
+
+from bs4 import BeautifulSoup
+from nltk.corpus import stopwords
+from nltk.tokenize import TweetTokenizer
+import re ,csv
+
+#se eliminan stopwords
+stoplist = stopwords.words('english')
+
+def preprocesamiento(texto):
+	nuevo_texto = re.sub(r"http\S+", "", texto)
+
+	nuevo_texto = BeautifulSoup(nuevo_texto).text
+	#nuevo_texto = BeautifulSoup(texto.decode('utf-8','ignore')).get_text()
+	nuevo_texto = nuevo_texto.lower()
+
+	tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
+	tokens = tknzr.tokenize(nuevo_texto)
+	token_sin_stopwords = [ token for token in tokens  if token not in stoplist and len(token) != 1]
+
+	token_sin_stopwords = [ re.sub('([\W_]+)', '', token) for token in token_sin_stopwords ]  
+
+	return token_sin_stopwords
+##revisar el preprocesar
+
+
+folder = "../NRC-Sentiment-Emotion-Lexicons/Lexicons/"
+archivo = pd.read_csv(folder+"NRC-Hashtag-Emotion-Lexicon-v0.2/NRC-Hashtag-Emotion-Lexicon-v0.2.txt",sep='\t',header=None)
+
+dicc_Emolex = {}
+for indice in range(archivo.shape[0]):
+	emocion,palabra,score= archivo.ix[indice]
+
+	if palabra in dicc_Emolex.keys():
+		dicc_Emolex[palabra].append([emocion,score])
+
+	else:
+		dicc_Emolex[palabra] = [[emocion,score]]
+##dicc emoclex creado!
+
+
+f = []
+folder = "tweets/"
+for (dirpath, dirnames, filenames) in walk(folder):
+	f.extend(filenames)
+	break
+
+
+csvFile = open('resultados.csv', 'w')
+#Use csv writer
+csvWriter = csv.writer(csvFile,quoting=csv.QUOTE_NONNUMERIC)
+csvWriter.writerow(["screen_name", "sadness score", "disgust score", "anger score", "surprise score", "cantidad tweets"])
+
+emociones_contrarias = [["sadness","joy"]]
+
+#para cada uusario dentro de la carpeta
+for historia_tweets in f: 
+	screen_name = historia_tweets[:-4]
+	print "para usuario: ",screen_name
+
+	historial = open(folder+historia_tweets)
+	historial = pd.read_csv(folder+historia_tweets)
+	cantidad_tweets = historial.shape[0]
+
+	score_usuario = {'sadness':[],'disgust':[],'anger':[],'surprise':[]}
+	for tweet in historial["text"].values: #por cada tweet
+		#ide,fav_count,ret_count, text, created = tweet.split(',')
+
+		tweet_procesado = preprocesamiento(tweet)
+		
+		score_tweet = {'anticipation':[],'joy':[],'fear':[],'disgust':[],'anger':[],'trust':[],'surprise':[],'sadness':[]}
+		for palabra in tweet_procesado: #si no tiene palabras el tweet? (se elimina todo)
+			if palabra in dicc_Emolex.keys():
+				for emocion,score in dicc_Emolex[palabra]: #elementos guardados en emolex
+					score_tweet[emocion].append(score)
+
+		#promediar por tweet (metrica)
+		for emocion1,emocion2 in emociones_contrarias:
+			if score_tweet[emocion1] == [] and score_tweet[emocion2] == []:
+				metrica = np.nan
+
+			else:#no nan
+				if score_tweet[emocion1] == []: #tweet no tiene emocion1
+					score_tweet[emocion1] == [0]
+				if score_tweet[emocion2] == []:
+					score_tweet[emocion2] == [0]
+
+				#meter lo de la metrica
+				metrica = np.mean(score_tweet[emocion1]) - np.mean(score_tweet[emocion2]) 
+
+			score_usuario[emocion1].append( metrica )  #cada tweet tiene una metrica
+
+	print score_usuario
+	#promediar las emociones de todos los tweet del usuario
+	for emocion in score_usuario.keys():
+		score_usuario[emocion] = np.nanmean(score_usuario[emocion])
+		print "Para la emocion %s tiene un score de %f"%(emocion,score_usuario[emocion])
+
+	#escribir resultado
+	csvWriter.writerow([screen_name, score_usuario["sadness"], score_usuario["disgust"], score_usuario["anger"], score_usuario["surprise"], cantidad_tweets])
+csvFile.close()
---
+++ recuperador.py
@@ -0,0 +1,116 @@
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+import json,csv,time, tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+
+#consumer key, consumer secret, access token, access secret.
+ckey="gQ1RX1eqCRS39n1dmL9nVzAEp"
+csecret="iMiZA2xXC4o7TsypLHj22HP1R0SAdfYqxYgFDKU33mra2SGhyA"
+atoken="320425023-iyx7eywe75Po9VtkaOkhcrlhXGFTwzrYYq55xcoH"
+asecret="4GAlKtNyS0Z4SIOdo4DnlFsBl8KlfczOhGy8I4yByF1Tb"
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+api = tweepy.API(auth)
+
+"""
+
+#searched_tweets = [status for status in tweepy.Cursor(api.search, q=query).items(max_tweets)]
+
+#searched_tweets = api.search(q="trump",result_type= 'mixed',truncated=False, show_user=True,count=10)
+
+searched_tweets = [status for status in tweepy.Cursor(api.search, q="sad", since = "2017-01-01", lang="en",show_user=True).items(1)]
+#print(json.dumps(searched_tweets[0]._json, indent=2)) 
+
+#json.dumps(status._json)
+#print(json.dumps(searched_tweets[0]._json, indent=2)) 
+
+print "datos rescatados:"
+print "Id user:", searched_tweets[0].user.id
+print "Nombre:", searched_tweets[0].user.name
+print "Screen name;" ,searched_tweets[0].user.screen_name
+print "Descripcion:", searched_tweets[0].user.description
+print "Location:", searched_tweets[0].user.location
+print "Fecha:", searched_tweets[0].created_at
+print "Tweet:" ,searched_tweets[0].text
+
+# Open/create a file to append data to
+csvFile = open('result.csv', 'a')
+#Use csv writer
+csvWriter = csv.writer(csvFile,quoting=csv.QUOTE_NONNUMERIC)
+csvWriter.writerow(["id user", "name","screen name","description","location","date","tweet"])
+
+
+query1 = "lonely" or "bipolar" or "crazy" or "suicide" or "end" or "sorry" or "hopelessness" or "sad" or "alone" or "damage" or "give up" or "rage" or "dissapoint"or "mind" or "aggression" or "anger" or "fear" or "cares" or"nobody" or "oppression"or "feel" or "break" or "pornography" or "defrauded" or "fugitive"or "murder" or "heart" or "arrest" or "kill" or "crime" or "death" or "substance"
+query2= "weapon" or "pain" or "suicidal" or "depression" or "anxiety" or "mental"or "depressed" or "i hate happy" or "sad today" or "sad day" or "jail" or "so mad"or "cannot live" or "not have love" or "stupid people" or "bad people" or "mad internet" or "hate living here" or "nobody understand"
+query = query1 or query2
+max_tweets = 10000
+
+date = time.strftime("%Y-%m-%d")
+for status in tweepy.Cursor(api.search, q=query, until ="2017-06-23", 
+				lang="en",show_user=True, geocode= "35.1466548,-93.626001,700mi").items(max_tweets):
+	#valores a guardar
+	csvWriter.writerow([status.user.id,
+						status.user.name,
+						status.user.screen_name,
+						status.user.description,
+						status.user.location,
+						status.created_at,
+						status.text.encode('utf-8').strip()])
+						#encode('utf-8').strip()])#.encode('utf-8')])
+csvFile.close()
+
+""" 
+
+
+file = open("diccionario_usuarios.txt","a") 
+ 
+screen_name = "asd"
+
+#for ides in ids:
+# formato id usuario
+csvFile = open("tweets/"+screen_name+'.csv', 'a')
+#Use csv writer
+csvWriter = csv.writer(csvFile,quoting=csv.QUOTE_NONNUMERIC)
+csvWriter.writerow(["id", "favorite count","retweet count","text","created at"])
+max_tweets = 50000
+
+usuario = api.get_user(screen_name = screen_name)
+file.write(screen_name+ " "+usuario.name+ " "+str(usuario.id)+"\n") 
+file.close()
+
+#para rescatar info de usuario
+for status in tweepy.Cursor(api.user_timeline, screen_name=screen_name).items(max_tweets):
+	#print(json.dumps(status._json, indent=2)) 
+	
+	if status.text[:2] != 'RT': #si el tweet lo escribio el
+		csvWriter.writerow([status.id,
+			status.favorite_count,
+			status.retweet_count,
+			status.text,
+			status.created_at
+			])
+
+#API.user_timeline([id/user_id/screen_name][, since_id][, max_id][, count][, page])
+
+csvFile.close()
+
+"""
+class listener(StreamListener):
+
+    def on_data(self, data):
+        print(data)
+        return(True)
+
+    def on_error(self, status):
+        print status
+#twitterStream = Stream(auth, listener())
+#twitterStream.filter(track=["car"])
+
+"""
